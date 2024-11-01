1557b8e48f8384da3a5e738ee40cfa7eb90cd3d5
William Wollert <williamwollert@gmail.com>
2024-11-01 17:12:35 -0300

    v1.4 pgbd setup

---
+++ sql/setup.sql
@@ -0,0 +1,20 @@
+--host=localhost port=5432 dbname=projetohospital user=postgres password=xxxxxxx
+
+CREATE TABLE avaliacoes (
+    id integer PRIMARY KEY;
+    nota integer NOT NULL;
+    data_avaliacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+CREATE TABLE perguntas (
+	id serial PRIMARY KEY,
+	pergunta varchar NOT NULL,
+	data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE dispositivos (
+	id SERIAL PRIMARY KEY,
+	nome_dispo varchar NOT NULL,
+	setor_id integer NOT NULL
+); 
