046f1259f73e316a6d43271a73f86882a99b25df
William Wollert <williamwollert@gmail.com>
2024-11-01 11:40:45 -0300

    v1.3.1 css obrigado

--- public/php/obrigado.php
+++ public/php/obrigado.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="William Wollert - BSN T28 - UNIDAVI">
     <meta name="description" content="Trabalho semestral de Desenvolvimento WEB 1 - UNIDAVI">
     <meta name="keywords" content="HRAV, CSS, HTML, PHP, SQL, UNIDAVI, BSN">
     <title>Obrigado - HRAV</title>
-    <link rel="stylesheet" type="text/css" href="css/styles2.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles2.css">
 </head>
-    <body>
-        <main>
-            <div class="container">
-                <div class="caixadetexto">
-                    <h1 id="texto1">Agradeçemos sua Avaliação</h1>
-                </div>
+<body>
+    <main>
+        <div class="container">
+            <div class="caixa-texto">
+                <h1 id="titulo">Agradecemos sua Avaliação!</h1>
+                <p id="texto">O Hospital Regional Alto Vale (HRAV) agradece sua resposta e ela é muito importante para nós, pois nos ajuda a melhorar continuamente nossos serviços.</p>
             </div>
-        </main>
-    </body>
+        </div>
+    </main>
+</body>
 </html>
